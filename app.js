6a9157bd087ced9a191ad218ba737f11e1b70e83
sangitaguha <sangitaguho@gmail.com>
2021-04-15 14:26:06 +0600

    all index.html&app. js

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
-// function loadData(){
-//   fetch ("https://jsonplaceholder.typicode.com/users")
-//   .then (res => res.json ())
+function loadData(){
+  fetch ("https://jsonplaceholder.typicode.com/users")
+  .then (res => res.json ())
 
-//   .then (data => {
-//     displayData(data);
-//   })
+  .then (data => {
+    displayData(data);
+  })
     
-// }
-// loadData;
-
-
-
+}
+loadData;
 
 function displayData(data){
-  const ul = document.getElementById('item');
-  for (let i = 0; i < meals.length; i++){
-      const item = items [i];
-      const itemForm = document.createElement('form');
-      const object= document.createElement('object'); 
-      item.innerText = meal.object;
-      const form = document.createElement('form');
-      form.innerText = meal.form;
+  console.log(data);
+}
 
-      mealForm.appendChild(object);
-      mealForm.appendChild(form);
- 
-  }  
-}
-function displayUl( ul){
-  console.log(ul);
-}
+
 
